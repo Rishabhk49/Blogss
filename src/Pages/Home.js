94fa745e7a667e2849b30b986216a41593899f1e
Rishabhk49 <Rishabhkatiyar050@gmail.com>
2024-06-14 11:53:45 +0530

    first commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../components/Header'
+import Blogs from '../components/Blogs'
+import Pagination from '../components/Pagination'
+
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <Blogs />
+            <Pagination />
+        </div> 
+    )
+}
+
+export default Home
